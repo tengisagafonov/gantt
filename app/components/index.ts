8aba506436f9b1ddb6d2fa81b72b9e82398e149d
admin <TengisCody@cody.mn>
2022-12-12 08:58:37 +0800

    Small Changes

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,4 @@
 import {PhotoSwiper} from 'app/components/photoswiper';
-import {StickyHeader} from 'app/components/stickyHeader';
 import Header from 'components/header';
 
-export {PhotoSwiper, Header, StickyHeader};
+export {PhotoSwiper, Header};
