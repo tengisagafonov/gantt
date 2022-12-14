f65e111d71e682518def6794d008951e0bc3115a
admin <TengisCody@cody.mn>
2022-12-14 20:38:21 +0800

    Add animation to TabView

--- app/screens/shopInfo/components/tabItem/index.tsx
+++ app/screens/shopInfo/components/tabItem/index.tsx
@@ -1,54 +1,71 @@
 import React, {FC} from 'react';
 import {
-  View,
-  Text,
+  Animated,
   StyleSheet,
-  ViewStyle,
+  Text,
   TouchableOpacity,
+  View,
+  ViewStyle,
 } from 'react-native';
 import {
   NavigationState,
   SceneRendererProps,
 } from 'react-native-tab-view/lib/typescript/src/types';
-import {Colors, spacing} from 'config/Theme';
+import {Colors, fontWeight, spacing} from 'config/Theme';
 
 type ITabItemProps = SceneRendererProps & {
   navigationState: NavigationState<{key: string; title: string}>;
 };
 
+const getTranslateX = (
+  scrollAmount: Animated.Value,
+  maxScrollDistance: number,
+) => Animated.multiply(scrollAmount, maxScrollDistance);
+
 const TabItem: FC<ITabItemProps> = props => {
   const {routes, index} = props.navigationState;
-  const {jumpTo} = props;
+  const {jumpTo, position, layout} = props;
+
   return (
     <View style={styles.tab}>
       {routes.map((i: {title: string; key: string}, idx: number) => (
         <TouchableOpacity
           key={idx}
           onPressIn={() => jumpTo(i.key)}
-          style={itemStyle(index === idx)}>
+          style={styles.itemStyle}>
           <Text style={text(index === idx)}>{i.title}</Text>
         </TouchableOpacity>
       ))}
+      <Animated.View style={animated(position, layout.width / routes.length)} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   tab: {flexDirection: 'row', justifyContent: 'space-around'},
-});
-
-const itemStyle = (active: boolean) =>
-  ({
+  itemStyle: {
     flex: 1,
     alignItems: 'center',
     paddingVertical: spacing.medium,
-    color: active ? Colors.white : Colors.black,
-    backgroundColor: active ? Colors.buttonActiveBlue : Colors.white,
-  } as ViewStyle);
+    color: Colors.white,
+    backgroundColor: Colors.white,
+  },
+});
+
+const animated = (position: Animated.Value, width: number) =>
+  ({
+    position: 'absolute',
+    bottom: 0,
+    borderColor: Colors.buttonActiveBlue,
+    width: width,
+    borderWidth: 1,
+    transform: [{translateX: getTranslateX(position, width)}],
+  } as Animated.AnimatedProps<ViewStyle>);
 
 const text = (active: boolean) =>
   ({
-    color: active ? Colors.white : Colors.black,
+    fontWeight: active ? fontWeight.bold : fontWeight.medium,
+    color: active ? Colors.buttonActiveBlue : Colors.black,
   } as ViewStyle);
 
 export default TabItem;
